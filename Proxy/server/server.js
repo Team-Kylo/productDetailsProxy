0f7bb8789586dcbfea9ba1878c636d7fd74cc377
naszzz <naszagorov@gmail.com>
2020-02-05 20:14:29 -0800

    .env add to proxy server ports

--- Proxy/server/server.js
+++ Proxy/server/server.js
@@ -1,14 +1,23 @@
+/* eslint-disable no-undef */
 const express = require('express');
 const axios = require('axios');
 
 const app = express();
 const port = 3000;
 
+require('dotenv').config();
+
+
+reviewsPort = process.env.PORT_REVIEWS || 3001;
+detailsPort = process.env.PORT_DETAILS || 3002;
+imagesPort = process.env.PORT_IMAGES || 3003;
+itemsPort = process.env.PORT_ITEMS || 3004;
+
 app.use(express.static('./public'));
 
 app.get('/details/:id', (req, res) => {
   const id = req.params.id;
-  axios.get(`http://localhost:3002/details/${id}`)
+  axios.get(`http://localhost:${detailsPort}/details/${id}`)
     .then((data) => {
       res.status(200).json(data.data);
     });
@@ -16,7 +25,7 @@ app.get('/details/:id', (req, res) => {
 
 app.get('/items/:id', (req, res) => {
   const id = req.params.id;
-  axios.get(`http://localhost:3004/items/${id}`)
+  axios.get(`http://localhost:${itemsPort}/items/${id}`)
     .then((data) => {
       res.status(200).json(data.data);
     });
@@ -24,7 +33,7 @@ app.get('/items/:id', (req, res) => {
 
 app.get('/reviews/:id', (req, res) => {
   const id = req.params.id;
-  axios.get(`http://localhost:3001/reviews/${id}`)
+  axios.get(`http://localhost:${reviewsPort}/reviews/${id}`)
     .then((data) => {
       res.status(200).json(data.data);
     })
