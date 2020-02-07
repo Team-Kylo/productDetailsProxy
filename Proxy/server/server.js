86fa0164a5dde2f372c8b68c974a5d6719084891
naszzz <naszagorov@gmail.com>
2020-02-07 15:15:23 -0800

    commit

--- Proxy/server/server.js
+++ Proxy/server/server.js
@@ -10,22 +10,22 @@ require('dotenv').config();
 
 reviewsPort = process.env.PORT_REVIEWS || 3001;
 detailsPort = process.env.PORT_DETAILS || 3002;
-imagesPort = process.env.PORT_IMAGES || 3003;
+carouselPort = process.env.PORT_CAROUSEL || 3003;
 itemsPort = process.env.PORT_ITEMS || 3004;
 
 app.use(express.static('./public'));
 
 app.get('/details/:id', (req, res) => {
   const id = req.params.id;
-  axios.get(`http://localhost:${detailsPort}/details/${id}`)
+  axios.get(`http://ec2-18-220-152-98.us-east-2.compute.amazonaws.com/details/${id}`)
     .then((data) => {
       res.status(200).json(data.data);
     });
 });
 
-app.get('/items/:id', (req, res) => {
+app.get('/additional/:id', (req, res) => {
   const id = req.params.id;
-  axios.get(`http://localhost:${itemsPort}/items/${id}`)
+  axios.get(`http://ec2-3-133-117-113.us-east-2.compute.amazonaws.com/additional/${id}`)
     .then((data) => {
       res.status(200).json(data.data);
     });
@@ -33,9 +33,20 @@ app.get('/items/:id', (req, res) => {
 
 app.get('/reviews/:id', (req, res) => {
   const id = req.params.id;
-  axios.get(`http://localhost:${reviewsPort}/reviews/${id}`)
+  axios.get(`http://ec2-3-14-246-138.us-east-2.compute.amazonaws.com/reviews/${id}`)
     .then((data) => {
       res.status(200).json(data.data);
+    })
+})
+
+app.get('/carousel/:id', (req, res) => {
+  const id = req.params.id;
+  axios.get(`http://localhost:${carouselPort}/carousel/${id}`)
+    .then((data) => {
+      res.status(200).json(data.data);
+    })
+    .catch((err) => {
+      console.log(err);
     })
 })
 
